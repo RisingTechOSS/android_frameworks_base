9f37d6b7b57823ac22d17519d6741d749d01f14f
Pranav Vashi <neobuddy89@gmail.com>
2017-07-19 23:46:45 +0530

    SystemUI: Allow using 4G icon instead LTE [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -49,6 +49,7 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.settingslib.AccessibilityContentDescriptions;
 import com.android.settingslib.SignalIcon.MobileIconGroup;
 import com.android.settingslib.graph.SignalDrawable;
+import com.android.settingslib.mobile.MobileMappings;
 import com.android.settingslib.mobile.MobileMappings.Config;
 import com.android.settingslib.mobile.MobileStatusTracker;
 import com.android.settingslib.mobile.MobileStatusTracker.MobileStatus;
@@ -83,6 +84,8 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
 
     private static final String ROAMING_INDICATOR_ICON =
             "system:" + Settings.System.ROAMING_INDICATOR_ICON;
+    private static final String SHOW_FOURG_ICON =
+            "system:" + Settings.System.SHOW_FOURG_ICON;
 
     private final TelephonyManager mPhone;
     private final CarrierConfigTracker mCarrierConfigTracker;
@@ -257,6 +260,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         mProviderModelBehavior = SystemProperties.getBoolean("persist.sys.flags.combined_signal_icons", true);
 
         Dependency.get(TunerService.class).addTunable(this, ROAMING_INDICATOR_ICON);
+        Dependency.get(TunerService.class).addTunable(this, SHOW_FOURG_ICON);
     }
 
     @Override
@@ -266,6 +270,11 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                 mRoamingIconAllowed =
                     TunerService.parseIntegerSwitch(newValue, true);
                 updateTelephony();
+                break;
+            case SHOW_FOURG_ICON:
+                mConfig = Config.readConfig(mContext);
+                setConfiguration(mConfig);
+                notifyListeners();
                 break;
             default:
                 break;
