7ae12effe33012bcec58e0f70e32677158da199f
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2021-03-20 10:35:14 +0000

    SystemUI: Fix status bar and QS margins and paddings
    Last revised on 2022/09/24, targeting T
    
    The way I think SB/QS margins/paddings should work:
    - Devices with left notch: [notch_definition][status_bar_padding_start][content]...[content][status_bar_padding_end][rounded_corner_content_padding]
    - Devices with center or no notch: [rounded_corner_content_padding][status_bar_padding_start][content]...[content][status_bar_padding_end][rounded_corner_content_padding]
    Key point being:
    - SB (including keyguard) and QS should always act uniformly
    - Notch definition should only be the notch itself, without additional padding
    - Instead, these paddings should be covered by status_bar_padding_{start|end}
    As a result, below changes have been made:
    - Change keyguard_carrier_text_margin into a padding
    - Link keyguard paddings to status_bar_padding_{start|end}
    - Add status_bar_padding_{start|end} to quick settings header
    - Remove several unnecessary margins and paddings
    - Animate padding for new QS clock in s-qpr1
    
    [ghostrider-reborn: adapt to combined qs headers and fix for rtl]
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Ic91fa398813e1907297bb0892c444d96405950e7

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -138,6 +138,8 @@ constructor(
     private val shadeCarrierText: TextView? = header.findViewById(R.id.shade_carrier_text)
     private val shadeNoCarrierText: TextView? = header.findViewById(R.id.shade_carrier_text)
 
+    private var sbPaddingLeft = 0
+    private var sbPaddingRight = 0
     private var roundedCorners = 0
     private var cutout: DisplayCutout? = null
     private var lastInsets: WindowInsets? = null
@@ -390,8 +392,8 @@ constructor(
         val cutout = insets.displayCutout.also { this.cutout = it }
 
         val sbInsets: Pair<Int, Int> = insetsProvider.getStatusBarContentInsetsForCurrentRotation()
-        val cutoutLeft = sbInsets.first
-        val cutoutRight = sbInsets.second
+        val cutoutLeft = sbInsets.first + sbPaddingLeft
+        val cutoutRight = sbInsets.second + sbPaddingRight
         val hasCornerCutout: Boolean = insetsProvider.currentRotationHasCornerCutout()
         updateQQSPaddings()
         // Set these guides as the left/right limits for content that lives in the top row, using
@@ -515,6 +517,8 @@ constructor(
 
     private fun updateResources() {
         roundedCorners = resources.getDimensionPixelSize(R.dimen.rounded_corner_content_padding)
+        sbPaddingLeft = resources.getDimensionPixelSize(R.dimen.status_bar_padding_start)
+        sbPaddingRight = resources.getDimensionPixelSize(R.dimen.status_bar_padding_end)
         val padding = resources.getDimensionPixelSize(R.dimen.qs_panel_padding)
         header.setPadding(padding, header.paddingTop, padding, header.paddingBottom)
         updateQQSPaddings()
