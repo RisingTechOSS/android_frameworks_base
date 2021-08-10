bde221d2fb574925f25120aeb7d7a4167cf44ce3
1582130940 <19632674+1582130940@users.noreply.github.com>
2021-08-10 07:26:39 +0200

    fwb: Only enable some features on eng build
    * ActivityManagerService: Disallow requestSystemServerHeapDump on non-eng builds
    jhenrique09 <jhenrique09.mcz@hotmail.com>
    
    * SystemServer: Only enable fd leak tracker on eng
    Simao Gomes Viana <xdevs23@outlook.com>
    
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Simao Gomes Viana <xdevs23@outlook.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -7290,7 +7290,7 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     @Override
     public void requestSystemServerHeapDump() {
-        if (!Build.IS_DEBUGGABLE) {
+        if (!Build.IS_ENG) {
             Slog.wtf(TAG, "requestSystemServerHeapDump called on a user build");
             return;
         }
