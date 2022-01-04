67e66e71b461285e9bf0a5fb53f8d2ad0f4c4a62
Kshitij Gupta <kshitijgm@gmail.com>
2022-01-04 16:10:15 +0530

    [SQUASH] fwb: Screen off animations [1/2]
    Fade (default), CRT, and Scale
    
    Credit and respect to xplodwild for paving the way
    back in the KitKat days!
    
    History of the ElectronBeam class can be found here
    https://github.com/DirtyUnicorns/android_frameworks_base/commits/kitkat/services/java/com/android/server/power/ElectronBeam.java
    
    *** Changes for Android 10 by bigrushdog ***
    Various updates needed for Surface related API in the ElectronBeam animation class
    
    [AgentFabulous | POSP]
    - Rebase and rewrite ElectronBeam class on top of ColorFade
    - Add usage of class-common Transaction instance
    - Add support for respecting wide-color and protected-content
    - Fixup for new API
    
    Change-Id: I58d269d44c901a8c0471807e3cf05c2054205d28
    Co-Authored-By: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Add upstream fixes to ElectronBeam screen-off animation
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Fix crash with protected content with ElectronBeam/Scale screen-off animation
    
    * Kind of an oddly specific use case here but to reproduce:
      - Enable CRT Screen Off Animation
      - Start playing a video from Netflix
      - Turn off screen using  power key, you should see the crash then
    
    Co-authored by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Matt Filetto <matt.filetto@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5697,6 +5697,12 @@ public final class Settings {
         public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
 
         /**
+         * Defines the screen-off animation to display
+         * @hide
+         */
+        public static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
