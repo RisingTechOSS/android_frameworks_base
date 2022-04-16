cef68b5c48adda94285977895d4e13142160c945
cjh1249131356 <cjh1249131356@gmail.com>
2022-04-16 11:41:31 +0800

    base: Always turn on sensors when disabling sleep mode
    * Also verify once to make sure sensors are turned on.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- core/java/com/android/internal/util/crdroid/Utils.java
+++ core/java/com/android/internal/util/crdroid/Utils.java
@@ -150,7 +150,6 @@ public class Utils {
         private static boolean mWifiState;
         private static boolean mCellularState;
         private static boolean mBluetoothState;
-        private static boolean mSensorState;
         private static int mRingerState;
         private static int mZenState;
 
@@ -334,7 +333,6 @@ public class Utils {
             final boolean disableSensors = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                     Settings.Secure.SLEEP_MODE_SENSORS_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
             if (disableSensors) {
-                mSensorState = isSensorEnabled();
                 setSensorEnabled(false);
             }
 
@@ -387,8 +385,14 @@ public class Utils {
             // Enable Sensors
             final boolean disableSensors = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                     Settings.Secure.SLEEP_MODE_SENSORS_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
-            if (disableSensors && mSensorState != isSensorEnabled()) {
-                setSensorEnabled(mSensorState);
+            if (disableSensors) {
+                setSensorEnabled(true);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {}
+                if (!isSensorEnabled()) {
+                    setSensorEnabled(true);
+                }
             }
 
             // Set Ringer mode (0: Off, 1: Vibrate, 2:DND: 3:Silent)
