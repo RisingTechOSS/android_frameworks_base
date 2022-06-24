82a8fe057981a5b84fd019644bb2e0868ac2f9db
Zemeng Li <zemeng.li@mediatek.com>
2022-06-24 14:25:00 +0800

    Fix NullPointerException to get sim accounts
    After the process "android.process.acore" is killed, the null value will be returned by ContentResolver when the api "getSimAccounts" is called, which will throw NullPointerException.
    
    Explicitly get a ContentProviderClient and call on it. When the process is killed, the RemoteException will be catched.
    
    Bug: 231687839
    Change-Id: Idb265d9fcc781612e571deae5014729e3ed59b44

--- core/java/android/provider/ContactsContract.java
+++ core/java/android/provider/ContactsContract.java
@@ -133,6 +133,7 @@ import java.util.Set;
  */
 @SuppressWarnings("unused")
 public final class ContactsContract {
+    private static final String TAG = ContactsContract.class.getSimpleName();
     /** The authority for the contacts provider */
     public static final String AUTHORITY = "com.android.contacts";
     /** A content:// style uri to the authority for the contacts provider */
@@ -8410,7 +8411,7 @@ public final class ContactsContract {
             extras.putString(KEY_ACCOUNT_NAME, accountName);
             extras.putString(KEY_ACCOUNT_TYPE, accountType);
 
-            contentResolver.call(ContactsContract.AUTHORITY_URI,
+            nullSafeCall(contentResolver, ContactsContract.AUTHORITY_URI,
                     ContactsContract.SimContacts.ADD_SIM_ACCOUNT_METHOD,
                     null, extras);
         }
@@ -8433,7 +8434,7 @@ public final class ContactsContract {
             Bundle extras = new Bundle();
             extras.putInt(KEY_SIM_SLOT_INDEX, simSlotIndex);
 
-            contentResolver.call(ContactsContract.AUTHORITY_URI,
+            nullSafeCall(contentResolver, ContactsContract.AUTHORITY_URI,
                     ContactsContract.SimContacts.REMOVE_SIM_ACCOUNT_METHOD,
                     null, extras);
         }
@@ -8445,7 +8446,7 @@ public final class ContactsContract {
          */
         public static @NonNull List<SimAccount> getSimAccounts(
                 @NonNull ContentResolver contentResolver) {
-            Bundle response = contentResolver.call(ContactsContract.AUTHORITY_URI,
+            Bundle response = nullSafeCall(contentResolver, ContactsContract.AUTHORITY_URI,
                     ContactsContract.SimContacts.QUERY_SIM_ACCOUNTS_METHOD,
                     null, null);
             List<SimAccount> result = response.getParcelableArrayList(KEY_SIM_ACCOUNTS);
@@ -9064,7 +9065,8 @@ public final class ContactsContract {
          * @param contactId the id of the contact to undemote.
          */
         public static void undemote(ContentResolver contentResolver, long contactId) {
-            contentResolver.call(ContactsContract.AUTHORITY_URI, PinnedPositions.UNDEMOTE_METHOD,
+            nullSafeCall(contentResolver, ContactsContract.AUTHORITY_URI,
+                    PinnedPositions.UNDEMOTE_METHOD,
                     String.valueOf(contactId), null);
         }
 
@@ -10275,5 +10277,20 @@ public final class ContactsContract {
          */
         public static final String CONTENT_ITEM_TYPE =
                 "vnd.android.cursor.item/contact_metadata_sync_state";
+    }
+
+    private static Bundle nullSafeCall(@NonNull ContentResolver resolver, @NonNull Uri uri,
+            @NonNull String method, @Nullable String arg, @Nullable Bundle extras) {
+        Bundle out = new Bundle();
+        ContentProviderClient client = resolver.acquireContentProviderClient(uri);
+        if (client == null) {
+            return out;
+        }
+        try (client) {
+            return client.call(method, arg, extras);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Failed to call ContactsProvider with exception " + e);
+            return out;
+        }
     }
 }
