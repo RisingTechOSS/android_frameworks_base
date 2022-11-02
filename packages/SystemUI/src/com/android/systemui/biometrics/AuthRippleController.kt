528c98f43dd072089cf43854e76b69d881951db7
Chenyang Zhong <zhongcy95@gmail.com>
2022-11-02 13:34:55 +0800

    AuthRippleController: Only update sensor location when necessary
    The variable circleReveal is instantiated with a new CircleReveal
    object every time updateSensorLocation() is called. This causes
    an issue in the revealAnimator because its update listener checks
    if lightRevealScrim.revealEffect != circleReveal. If the sensor
    location is updated in the middle of a reveal animation, the
    circleReveal becomes a different object, thus triggering the return
    immediately and skipping the revealAmount update.
    
    If the revealAnimator finishes without updating revealAmount to 1f,
    the notification panel and heads-up/bubble background will be left
    with a radial black-white gradient patten. This can be triggered
    by unlocking with the fingerprint sensor into a landscape app
    because the portrait (keyguard) to landscape (app) transition calls
    updateSensorLocation(), during which the unlock ripple reveal is
    also happening.
    
    On most devices, the sensor location is not actually changing
    during run time, regardless of portrait or landscape screen
    orientation, so we should check the current value before creating
    a new CircleReveal object. This fixes most of the unupdated
    revealAmount cases. Also, change the type of circleReveal from
    LightRevealEffect to CircleReveal so that centerX and centerY can
    be accessed directly. The variable circleReveal is never used for
    other types of reveal effect anyway.
    
    Test: put an app in landscape, lock the screen, unlock with a
          physical fingerprint sensor, and check the notification panel
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I7ed5fb25d2287d95b0343e4fa989bab3c9c7c360
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleController.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleController.kt
@@ -82,7 +82,7 @@ class AuthRippleController @Inject constructor(
     var lightRevealScrimAnimator: ValueAnimator? = null
     var fingerprintSensorLocation: Point? = null
     private var faceSensorLocation: Point? = null
-    private var circleReveal: LightRevealEffect? = null
+    private var circleReveal: CircleReveal? = null
 
     private var udfpsController: UdfpsController? = null
     private var udfpsRadius: Float = -1f
@@ -132,6 +132,7 @@ class AuthRippleController @Inject constructor(
         if (biometricSourceType == BiometricSourceType.FINGERPRINT) {
             fingerprintSensorLocation?.let {
                 mView.setFingerprintSensorLocation(it, udfpsRadius)
+            if (circleReveal == null || circleReveal!!.centerX != it.x || circleReveal!!.centerY != it.y) {
                 circleReveal = CircleReveal(
                         it.x,
                         it.y,
@@ -141,6 +142,7 @@ class AuthRippleController @Inject constructor(
                                 Math.max(it.y, centralSurfaces.displayHeight.toInt() - it.y)
                         )
                 )
+                }
                 showUnlockedRipple()
             }
         } else if (biometricSourceType == BiometricSourceType.FACE) {
