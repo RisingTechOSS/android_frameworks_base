bf725415d74445c407fcfcbb56d892662a8cc000
minaripenguin <minaripenguin@users.noreply.github.com>
2023-01-26 05:49:14 +0800

    SystemUI: Lockscreen clock format settings [1/2]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6481,6 +6481,12 @@ public final class Settings {
         public static final String VOLUME_PANEL_HAPTIC_FEEDBACK = "volume_panel_haptic_feedback";
 
         /**
+         * Lockscreen clock format
+         * @hide
+         */
+        public static final String CLOCK_USE_CUSTOM_FORMAT = "clock_use_custom_format";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
