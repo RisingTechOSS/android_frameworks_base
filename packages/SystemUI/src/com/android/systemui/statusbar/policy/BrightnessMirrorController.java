vanpranav/platform_frameworks_base

BrightnessController: update icon state for auto bg icon - Change button color depending on state - Update off state icon from Amartya (@ yukiscape)
Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/policy/BrightnessMirrorController.java
@@ -224,6 +224,9 @@ public class BrightnessMirrorController
             iv.setImageResource(isAutomatic
                     ? com.android.systemui.R.drawable.ic_qs_brightness_auto_on_new
                     : com.android.systemui.R.drawable.ic_qs_brightness_auto_off_new);
+            iv.setBackgroundResource(isAutomatic
+                    ? com.android.systemui.R.drawable.bg_qs_brightness_auto_on
+                    : com.android.systemui.R.drawable.bg_qs_brightness_auto_off);
             iv.setVisibility(View.VISIBLE);
         } else {
             iv.setVisibility(View.GONE);

RisingTechOSS/android_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/policy/BrightnessMirrorController.java
@@ -216,8 +216,11 @@ public class BrightnessMirrorController
                     UserHandle.USER_CURRENT);
             boolean isAutomatic = automatic != Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL;
             mIcon.setImageResource(isAutomatic
-                    ? com.android.systemui.R.drawable.ic_qs_brightness_auto_on
-                    : com.android.systemui.R.drawable.ic_qs_brightness_auto_off);
+                    ? com.android.systemui.R.drawable.ic_qs_brightness_auto_on_new
+                    : com.android.systemui.R.drawable.ic_qs_brightness_auto_off_new);
+	    mIcon.setBackgroundResource(isAutomatic
+                    ? com.android.systemui.R.drawable.bg_qs_brightness_auto_on
+                    : com.android.systemui.R.drawable.bg_qs_brightness_auto_off);
             mIcon.setVisibility(View.VISIBLE);
         } else {
             mIcon.setVisibility(View.GONE);