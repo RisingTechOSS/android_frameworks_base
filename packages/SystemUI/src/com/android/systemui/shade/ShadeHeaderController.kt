e4927ff6297744ebcdfb3645d8f71bd7aba01505
Pranav Vashi <neobuddy89@gmail.com>
2023-04-01 08:42:16 +0530

    Battery Styles: Fix color on QS for circle battery styles
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -140,6 +140,7 @@ constructor(
 
     private var sbPaddingLeft = 0
     private var sbPaddingRight = 0
+    private var batteryStyle = batteryIcon.getBatteryStyle()
     private var roundedCorners = 0
     private var cutout: DisplayCutout? = null
     private var lastInsets: WindowInsets? = null
@@ -529,9 +530,14 @@ constructor(
         iconManager.setTint(fillColor)
         val textColor = Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary)
         val colorStateList = Utils.getColorAttr(context, android.R.attr.textColorPrimary)
-        if (textColor != textColorPrimary) {
-            val textColorSecondary = Utils.getColorAttrDefaultColor(context,
+        val currentBatteryStyle = batteryIcon.getBatteryStyle()
+        if (textColor != textColorPrimary || batteryStyle != currentBatteryStyle) {
+            var textColorSecondary = Utils.getColorAttrDefaultColor(context,
                     android.R.attr.textColorSecondary)
+            batteryStyle = currentBatteryStyle
+            if (batteryStyle == 1 || batteryStyle == 2 || batteryStyle == 3) {
+                textColorSecondary = Utils.getColorAttrDefaultColor(header.context, android.R.attr.textColorHint)
+            }
             textColorPrimary = textColor
             if (iconManager != null) {
                 iconManager.setTint(textColor)
