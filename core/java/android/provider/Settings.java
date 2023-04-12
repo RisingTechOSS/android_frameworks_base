6adb053f589ced87e1da7951ebe9e737c332e439
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-12 19:10:39 +0800

    settings: Add URI for dashboard styles
    Change-Id: Ie04c9ba46a3c11be84550bebd8bc78fb807df126
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5623,6 +5623,11 @@ public final class Settings {
         public static final String RINGTONE_VIBRATION_PATTERN = "ringtone_vibration_pattern";
 
         /**
+         * @hide
+         */
+        public static final String SETTINGS_DASHBOARD_STYLE = "settings_dashboard_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
