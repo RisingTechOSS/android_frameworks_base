b97eb09f09949a083ac46f298abb797a4546eb3a
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-23 22:14:17 +0800

    SystemUI: Improve dismiss all button visiblity
    * based from https://github.com/crdroidandroid/android_frameworks_base/commit/cbd1e922b8ffbe55617c3ef493d69f76a2497c5b but in our case the notification panel gets stuck sometimes especially when swiping qs panel after reticker appears
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I19450bafbfaa9ad40ba02d396de0076726e0e40a
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -2565,7 +2565,7 @@ public final class NotificationPanelViewController implements Dumpable {
         }
         mNotificationStackScrollLayoutController.setAlpha(alpha);
         if (mBarState != StatusBarState.KEYGUARD && !isFullyCollapsed() && !isPanelVisibleBecauseOfHeadsUp()) {
-            mCentralSurfaces.updateDismissAllVisibility(true);
+            mCentralSurfaces.updateDismissAllVisibility(mReTickerVisible != null && mReTickerVisible ? false : true);
         }
     }
 
@@ -5266,6 +5266,7 @@ public final class NotificationPanelViewController implements Dumpable {
         if (visibility && mReTickerComeback.getVisibility() == View.VISIBLE) {
             // check if we can dismiss reticker
             retickerDismiss();
+            mReTickerVisible = false;
         }
 
         if (visibility && getExpandedFraction() != 1) {
@@ -5315,16 +5316,19 @@ public final class NotificationPanelViewController implements Dumpable {
                     }
                     retickerDismiss();
                     reTickerViewVisibility();
+                    mReTickerVisible = false;
                 });
             }
         } else {
             retickerDismiss();
+            mReTickerVisible = false;
         }
     }
 
     protected void reTickerViewVisibility() {
         if (!mReTickerStatus) {
             retickerDismiss();
+            mReTickerVisible = false;
             return;
         }
 
@@ -5397,6 +5401,7 @@ public final class NotificationPanelViewController implements Dumpable {
                 if (mIsDismissRequested) {
                     // Dismiss was requested during animation, trigger dismiss animation
                     retickerDismiss(); 
+                    mReTickerVisible = false;
                 }
             }
         });
@@ -5448,7 +5453,6 @@ public final class NotificationPanelViewController implements Dumpable {
             public void onAnimationEnd(Animator animation) {
                 mReTickerComeback.setVisibility(View.GONE);
                 mNotificationStackScroller.setVisibility(View.VISIBLE);
-                mReTickerVisible = false;
                 mReTickerComeback.getViewTreeObserver().removeOnComputeInternalInsetsListener(mInsetsListener);
             }
         });
