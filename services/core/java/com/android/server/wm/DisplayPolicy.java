1c22f5a671e3536fec30265ba206b844488799ea
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-04 06:49:24 +0800

    Services: Boost gesture flings
    * like code linaro's gesture fling boosts
    
    [hydrangea]: update for sdk34 taking advantage of new swiping overrides , toned down boosting durations
    
    Co-authored-by: hydrangea07 <104114625+hydrangea07@users.noreply.github.com>
    Change-Id: I20db157b640c2c546730b7534f86d4465e597bb7
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -446,6 +446,10 @@ public class DisplayPolicy {
 
                 @Override
                 public void onSwipeFromTop() {
+                    if (mService.mPowerManagerInternal != null) {
+                        mService.mPowerManagerInternal.setPowerBoost(
+                                Boost.INTERACTION, 80);
+                    }
                     synchronized (mLock) {
                         requestTransientBars(mTopGestureHost,
                                 getControllableInsets(mTopGestureHost).top > 0);
@@ -454,6 +458,10 @@ public class DisplayPolicy {
 
                 @Override
                 public void onSwipeFromBottom() {
+                    if (mService.mPowerManagerInternal != null) {
+                        mService.mPowerManagerInternal.setPowerBoost(
+                                Boost.INTERACTION, 80);
+                    }
                     synchronized (mLock) {
                         requestTransientBars(mBottomGestureHost,
                                 getControllableInsets(mBottomGestureHost).bottom > 0);
@@ -467,6 +475,10 @@ public class DisplayPolicy {
 
                 @Override
                 public void onSwipeFromRight() {
+                    if (mService.mPowerManagerInternal != null) {
+                        mService.mPowerManagerInternal.setPowerBoost(
+                                Boost.INTERACTION, 80);
+                    }
                     final Region excludedRegion = Region.obtain();
                     synchronized (mLock) {
                         mDisplayContent.calculateSystemGestureExclusion(
@@ -482,6 +494,10 @@ public class DisplayPolicy {
 
                 @Override
                 public void onSwipeFromLeft() {
+                    if (mService.mPowerManagerInternal != null) {
+                        mService.mPowerManagerInternal.setPowerBoost(
+                                Boost.INTERACTION, 80);
+                    }
                     final Region excludedRegion = Region.obtain();
                     synchronized (mLock) {
                         mDisplayContent.calculateSystemGestureExclusion(
@@ -500,6 +516,32 @@ public class DisplayPolicy {
                     if (mService.mPowerManagerInternal != null) {
                         mService.mPowerManagerInternal.setPowerBoost(
                                 Boost.INTERACTION, duration);
+                    }
+                }
+
+                @Override
+                public void onVerticalFling(int duration) {
+                    if (mService.mPowerManagerInternal != null) {
+                        mService.mPowerManagerInternal.setPowerBoost(
+                                Boost.INTERACTION, duration + 80);
+                    }
+                }
+
+                @Override
+                public void onHorizontalFling(int duration) {
+                    if (mService.mPowerManagerInternal != null) {
+                        mService.mPowerManagerInternal.setPowerBoost(
+                               Boost.INTERACTION, duration + 80);
+                    }
+                }
+
+                @Override
+                public void onScroll(boolean started) {
+                    if (started) {
+                       if (mService.mPowerManagerInternal != null) {
+                           mService.mPowerManagerInternal.setPowerBoost(
+                                  Boost.DISPLAY_UPDATE_IMMINENT, 80);
+                       }
                     }
                 }
 
