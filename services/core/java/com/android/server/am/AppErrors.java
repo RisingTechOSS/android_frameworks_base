36a221e1f0436231bc4580a93da501efa31a17eb
chenxinyu7 <chenxinyu7@xiaomi.corp-partner.google.com>
2023-08-02 19:58:03 +0800

    Fix the problem that the ANR pop-up window keeps popping up
    Bug:294190887
    
    Change-Id: Icdd610a855ba4536aa26c66bfb8f6e3a95518177
    Signed-off-by: chenxinyu7 <chenxinyu7@xiaomi.corp-partner.google.com>

--- services/core/java/com/android/server/am/AppErrors.java
+++ services/core/java/com/android/server/am/AppErrors.java
@@ -1154,10 +1154,10 @@ class AppErrors {
 
             // Dismiss any ANR dialogs currently visible
             if (errState.getDialogController().hasAnrDialogs()) {
-                errState.setNotResponding(false);
-                errState.setNotRespondingReport(null);
                 errState.getDialogController().clearAnrDialogs();
             }
+            errState.setNotResponding(false);
+            errState.setNotRespondingReport(null);
             proc.mErrorState.setAnrData(null);
         }
     }
