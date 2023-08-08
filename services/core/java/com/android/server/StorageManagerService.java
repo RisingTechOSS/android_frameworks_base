0beace338e5d2fbf77c3e036d13fa809af3a34cf
minaripenguin <minaripenguin@users.noreply.github.com>
2023-08-08 23:14:16 +0800

    services: Optimize storage manager service
    * address TODOs and reduce verbosity for performance
    
    Change-Id: I1d470852dd23e723cdaef2ce664d756f434b31f4
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/StorageManagerService.java
+++ services/core/java/com/android/server/StorageManagerService.java
@@ -220,6 +220,9 @@ class StorageManagerService extends IStorageManager.Stub
 
     @GuardedBy("mLock")
     private final Set<Integer> mCeStoragePreparedUsers = new ArraySet<>();
+    
+    @GuardedBy("mLock")
+    private final ArrayMap<Integer, ArraySet<String>> mPackagesByUid = new ArrayMap<>();
 
     public static class Lifecycle extends SystemService {
         private StorageManagerService mStorageManagerService;
@@ -2109,14 +2112,22 @@ class StorageManagerService extends IStorageManager.Stub
 
     private void updateLegacyStorageApps(String packageName, int uid, boolean hasLegacy) {
         synchronized (mLock) {
+            mPackagesByUid.computeIfAbsent(uid, key -> new ArraySet<>()).add(packageName);
+
             if (hasLegacy) {
-                Slog.v(TAG, "Package " + packageName + " has legacy storage");
+                if (DEBUG_EVENTS) Slog.v(TAG, "Package " + packageName + " has legacy storage");
                 mUidsWithLegacyExternalStorage.add(uid);
             } else {
-                // TODO(b/149391976): Handle shared user id. Check if there's any other
-                // installed app with legacy external storage before removing
-                Slog.v(TAG, "Package " + packageName + " does not have legacy storage");
-                mUidsWithLegacyExternalStorage.remove(uid);
+                ArraySet<String> packages = mPackagesByUid.get(uid);
+                if (packages != null) {
+                    packages.remove(packageName);
+                    if (packages.isEmpty()) {
+                        mUidsWithLegacyExternalStorage.remove(uid);
+                        mPackagesByUid.remove(uid);
+                    }
+                } else {
+                    mUidsWithLegacyExternalStorage.remove(uid);
+                }
             }
         }
     }
@@ -2124,34 +2135,33 @@ class StorageManagerService extends IStorageManager.Stub
     private void snapshotAndMonitorLegacyStorageAppOp(UserHandle user) {
         int userId = user.getIdentifier();
 
-        // TODO(b/149391976): Use mIAppOpsService.getPackagesForOps instead of iterating below
-        // It should improve performance but the AppOps method doesn't return any app here :(
-        // This operation currently takes about ~20ms on a freshly flashed device
-        for (ApplicationInfo ai : mPmInternal.getInstalledApplications(MATCH_DIRECT_BOOT_AWARE
-                        | MATCH_DIRECT_BOOT_UNAWARE | MATCH_UNINSTALLED_PACKAGES | MATCH_ANY_USER,
-                        userId, Process.myUid())) {
-            try {
-                boolean hasLegacy = mIAppOpsService.checkOperation(OP_LEGACY_STORAGE, ai.uid,
-                        ai.packageName) == MODE_ALLOWED;
-                updateLegacyStorageApps(ai.packageName, ai.uid, hasLegacy);
-            } catch (RemoteException e) {
-                Slog.e(TAG, "Failed to check legacy op for package " + ai.packageName, e);
+        try {
+            List<AppOpsManager.PackageOps> packagesOps = mIAppOpsService.getPackagesForOps(new int[]{OP_LEGACY_STORAGE});
+            if (packagesOps != null) {
+                for (AppOpsManager.PackageOps packageOps : packagesOps) {
+                    String packageName = packageOps.getPackageName();
+                    int uid = packageOps.getUid();
+                    for (AppOpsManager.OpEntry entry : packageOps.getOps()) {
+                        boolean hasLegacy = entry.getMode() == MODE_ALLOWED;
+                        updateLegacyStorageApps(packageName, uid, hasLegacy);
+                    }
+                }
             }
+        } catch (RemoteException e) {
+            Slog.e(TAG, "Failed to get packages for ops", e);
         }
 
-        if (mPackageMonitorsForUser.get(userId) == null) {
+        mPackageMonitorsForUser.computeIfAbsent(userId, key -> {
             PackageMonitor monitor = new PackageMonitor() {
                 @Override
                 public void onPackageRemoved(String packageName, int uid) {
                     updateLegacyStorageApps(packageName, uid, false);
                 }
-            };
-            // TODO(b/149391976): Use different handler?
-            monitor.register(mContext, user, true, mHandler);
-            mPackageMonitorsForUser.put(userId, monitor);
-        } else {
-            Slog.w(TAG, "PackageMonitor is already registered for: " + userId);
-        }
+        };
+        // TODO(b/149391976): Use different handler?
+        monitor.register(mContext, user, true, mHandler);
+            return monitor;
+        });
     }
 
     private static long getLastAccessTime(AppOpsManager manager,
