84599d557e5172748dd2e4d307ac0da3217e8407
minaripenguin <minaripenguin@users.noreply.github.com>
2023-09-07 08:16:30 +0800

    BatteryStatsImpl: Fix divide by zero error
    logs:
    09-07 07:54:48.049  1542  1640 E BatteryExternalStatsWorker: java.lang.ArithmeticException: divide by zero
    09-07 07:54:48.049  1542  1640 E BatteryExternalStatsWorker: 	at com.android.internal.os.BatteryStatsImpl.noteModemControllerActivity(BatteryStatsImpl.java:13760)
    09-07 07:54:48.049  1542  1640 E BatteryExternalStatsWorker: 	at com.android.server.am.BatteryExternalStatsWorker.updateExternalStatsLocked(BatteryExternalStatsWorker.java:756)
    09-07 07:54:48.049  1542  1640 E BatteryExternalStatsWorker: 	at com.android.server.am.BatteryExternalStatsWorker.-$$Nest$mupdateExternalStatsLocked(Unknown Source:0)
    09-07 07:54:48.049  1542  1640 E BatteryExternalStatsWorker: 	at com.android.server.am.BatteryExternalStatsWorker$1.run(BatteryExternalStatsWorker.java:470)
    
    Change-Id: Ic13273965732e6eba5d33d0aa229b216da58af02
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/com/android/internal/os/BatteryStatsImpl.java
+++ core/java/com/android/internal/os/BatteryStatsImpl.java
@@ -13754,7 +13754,7 @@ public class BatteryStatsImpl extends BatteryStats {
             final SparseDoubleArray uidEstimatedConsumptionMah;
             final long dataConsumedChargeUC;
             if (consumedChargeUC > 0 && mMobileRadioPowerCalculator != null
-                    && mGlobalMeasuredEnergyStats != null) {
+                    && mGlobalMeasuredEnergyStats != null && totalRadioDurationMs != 0) {
                 // Crudely attribute power consumption. Added (totalRadioDurationMs / 2) to the
                 // numerator for long rounding.
                 final long phoneConsumedChargeUC =
