85a2a2d16eb5055756573247ffe0576e92e30c9e
703joko <703joko@gmail.com>
2023-12-26 19:24:52 +0530

    Introduce Lockscreen Clock Styles [1/2]
    all credits goes to @Corex1402 and @seklek
    
    mnri: few cleanups
    
    Change-Id: Id5ffe4f7dc482bc2d79557c85b3118904db52701
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

---
+++ packages/SystemUI/src/com/android/systemui/afterlife/ClockStyle.java
@@ -0,0 +1,143 @@
+package com.android.systemui.afterlife;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Handler;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextClock;
+import com.android.internal.util.crdroid.ThemeUtils;
+import com.android.systemui.Dependency;
+import com.android.systemui.res.R;
+import com.android.systemui.tuner.TunerService;
+
+public class ClockStyle extends RelativeLayout implements TunerService.Tunable {
+
+    private static final int[] CLOCK_VIEW_IDS = {
+            0,
+            R.id.keyguard_clock_style_oos,
+            R.id.keyguard_clock_style_ios,
+            R.id.keyguard_clock_style_cos,
+            R.id.keyguard_clock_style_custom,
+            R.id.keyguard_clock_style_custom1,
+            R.id.keyguard_clock_style_custom2,
+            R.id.keyguard_clock_style_custom3
+    };
+
+    private static final int DEFAULT_STYLE = 0; // Disabled
+    private static final String CLOCK_STYLE_KEY = "clock_style";
+
+    private static final String CLOCK_STYLE = "system:" + CLOCK_STYLE_KEY;
+
+    private ThemeUtils mThemeUtils;
+
+    private Context mContext;
+    private View[] clockViews;
+    private int mClockStyle;
+
+    private static final long UPDATE_INTERVAL_MILLIS = 15 * 1000;
+    private final Handler mHandler;
+    private long lastUpdateTimeMillis = 0;
+
+    public ClockStyle(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        mHandler = new Handler(context.getMainLooper());
+        mThemeUtils = new ThemeUtils(context);
+        Dependency.get(TunerService.class).addTunable(this, CLOCK_STYLE);
+    }
+
+    private void enableClockOverlays(boolean enable) {
+        boolean isCenterClock = mClockStyle == 2 ||
+                mClockStyle == 4 ||
+                mClockStyle == 5 ||
+                mClockStyle == 6;
+        mThemeUtils.setOverlayEnabled(
+                "android.theme.customization.smartspace",
+                enable ? "com.android.systemui.hide.smartspace" : "com.android.systemui",
+                "com.android.systemui");
+        mThemeUtils.setOverlayEnabled(
+                "android.theme.customization.smartspace_offset",
+                enable && isCenterClock
+                        ? "com.android.systemui.smartspace_offset.smartspace"
+                        : "com.android.systemui",
+                "com.android.systemui");
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mHandler.post(() -> {
+            clockViews = new View[CLOCK_VIEW_IDS.length];
+            for (int i = 0; i < CLOCK_VIEW_IDS.length; i++) {
+                if (CLOCK_VIEW_IDS[i] != 0) {
+                    clockViews[i] = findViewById(CLOCK_VIEW_IDS[i]);
+                } else {
+                    clockViews[i] = null;
+                }
+            }
+            updateClockView();
+        });
+    }
+
+    private void updateTextClockViews(View view) {
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            mHandler.post(() -> {
+                for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                    View childView = viewGroup.getChildAt(i);
+                    updateTextClockViews(childView);
+                    if (childView instanceof TextClock) {
+                        ((TextClock) childView).refreshTime();
+                    }
+                }
+            });
+        }
+    }
+
+    public void onTimeChanged() {
+        long currentTimeMillis = System.currentTimeMillis();
+        if (currentTimeMillis - lastUpdateTimeMillis >= UPDATE_INTERVAL_MILLIS) {
+            if (clockViews != null) {
+                mHandler.post(() -> {
+                    for (View clockView : clockViews) {
+                        updateTextClockViews(clockView);
+                        lastUpdateTimeMillis = currentTimeMillis;
+                    }
+                });
+            }
+        }
+    }
+
+    private void updateClockView() {
+        if (clockViews != null) {
+            mHandler.post(() -> {
+                for (int i = 0; i < clockViews.length; i++) {
+                    if (clockViews[i] != null) {
+                        int visibility = (i == mClockStyle) ? View.VISIBLE : View.GONE;
+                        if (clockViews[i].getVisibility() != visibility) {
+                            clockViews[i].setVisibility(visibility);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        switch (key) {
+            case CLOCK_STYLE:
+                mClockStyle = TunerService.parseInteger(newValue, 0);
+                updateClockView();
+                enableClockOverlays(mClockStyle != 0);
+                break;
+            default:
+                break;
+        }
+    }
+}
