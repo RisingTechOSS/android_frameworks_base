02780e27862edfa7c0b957073e9f4e1e4da2fb7d
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-20 13:00:37 +0800

    services: Introduce Three finger swipe gestures [1/2]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1128,7 +1128,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.System.LOCKSCREEN_ENABLE_POWER_MENU), true, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.THREE_FINGER_GESTURE), false, this,
+                    "three_finger_gesture_action"), false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.HARDWARE_KEYS_DISABLE), false, this,
@@ -3465,7 +3465,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
 
             boolean threeFingerGesture = Settings.System.getIntForUser(resolver,
-                    Settings.System.THREE_FINGER_GESTURE, 0, UserHandle.USER_CURRENT) == 1;
+                    "three_finger_gesture_action", 0, UserHandle.USER_CURRENT) != 0;
             if (mSwipeToScreenshot != null) {
                 if (haveEnableGesture != threeFingerGesture) {
                     haveEnableGesture = threeFingerGesture;
@@ -6984,12 +6984,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         
         GestureCallbacks gestureCallbacks = new GestureCallbacks(mContext, mCurrentUserId);
 
-        mSwipeToScreenshot = new SwipeToScreenshotListener(mContext, new SwipeToScreenshotListener.Callbacks() {
-            @Override
-            public void onSwipeThreeFinger() {
-                interceptScreenshotChord(TAKE_SCREENSHOT_FULLSCREEN, SCREENSHOT_KEY_OTHER, 0 /*pressDelay*/);
-            }
-        });
+        mSwipeToScreenshot = new SwipeToScreenshotListener(mContext, (SwipeToScreenshotListener.Callbacks) gestureCallbacks);
         
         mShakeGestures = ShakeGestureService.getInstance(mContext, (ShakeGestureService.ShakeGesturesCallbacks) gestureCallbacks);
         mShakeGestures.onStart();
