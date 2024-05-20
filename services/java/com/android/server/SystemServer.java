aaeb92c03ba81b9aa5d9b9d5466883c98e8d8c94
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-21 06:17:50 +0800

    services: Make legacy pocket mode no-op
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -185,8 +185,6 @@ import com.android.server.pm.dex.OdsignStatsLogger;
 import com.android.server.pm.permission.PermissionMigrationHelper;
 import com.android.server.pm.permission.PermissionMigrationHelperImpl;
 import com.android.server.pm.verify.domain.DomainVerificationService;
-import com.android.server.pocket.PocketBridgeService;
-import com.android.server.pocket.PocketService;
 import com.android.server.policy.AppOpsPolicy;
 import com.android.server.policy.PermissionPolicyService;
 import com.android.server.policy.PhoneWindowManager;
@@ -2664,17 +2662,6 @@ public final class SystemServer implements Dumpable {
             t.traceBegin("StartCrossProfileAppsService");
             mSystemServiceManager.startService(CrossProfileAppsService.class);
             t.traceEnd();
-
-            t.traceBegin("StartPocketService");
-            mSystemServiceManager.startService(PocketService.class);
-            t.traceEnd();
-
-            if (!context.getResources().getString(
-                    com.android.internal.R.string.config_pocketBridgeSysfsInpocket).isEmpty()) {
-                t.traceBegin("StartPocketBridgeService");
-                mSystemServiceManager.startService(PocketBridgeService.class);
-                t.traceEnd();
-            }
 
             t.traceBegin("StartPeopleService");
             mSystemServiceManager.startService(PeopleService.class);
