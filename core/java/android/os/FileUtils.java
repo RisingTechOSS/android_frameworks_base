01ca8f90f72341e013b1ea9a6581dac920ef6c3b
minaripenguin <minaripenguin@users.noreply.github.com>
2024-07-28 09:58:25 +0800

    FileUtils: Silence Logspam
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/os/FileUtils.java
+++ core/java/android/os/FileUtils.java
@@ -1422,7 +1422,7 @@ public final class FileUtils {
             }
         }
 
-        Log.d(TAG, String.format("Rounded bytes from %d to %d", size, val * pow));
+        //Log.d(TAG, String.format("Rounded bytes from %d to %d", size, val * pow));
         return val * pow;
     }
 
