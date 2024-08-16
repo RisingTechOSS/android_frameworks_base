189a39bacb0864220449f918bc2f120ee96b3ac5
linkai <linkai@xiaomi.com>
2024-08-16 22:02:32 +0800

    Fix the freeform window flicker issues when playing HDR videos.
    Bug:360380695
    Test:Manual
    
    Change-Id: I2d70dc94c82bea85aff138a8d621d7712bd82d75
    Signed-off-by: linkai <linkai@xiaomi.com>

--- services/core/java/com/android/server/wm/Transition.java
+++ services/core/java/com/android/server/wm/Transition.java
@@ -3679,7 +3679,7 @@ class Transition implements BLASTSyncEngine.TransactionReadyListener {
                             .setSourceCrop(cropBounds)
                             .setCaptureSecureLayers(true)
                             .setAllowProtected(true)
-                            .setHintForSeamlessTransition(isDisplayRotation)
+                            .setHintForSeamlessTransition(true)
                             .build();
             ScreenCapture.ScreenshotHardwareBuffer screenshotBuffer =
                     ScreenCapture.captureLayers(captureArgs);
