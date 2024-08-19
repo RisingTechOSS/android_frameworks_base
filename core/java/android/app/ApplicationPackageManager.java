7ac398ef7278d810e68d4299ae8b8fcc5fd75e2e
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-19 17:56:10 +0800

    Revert PixelPropUtils conflicts [SQUASH]
    Revert "PixelPropsUtils: Update for pixel code names"
    
    This reverts commit 9bb634c97b94c836d378f21df63723e2453776de.

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -833,11 +833,6 @@ public class ApplicationPackageManager extends PackageManager {
             };
 
     private static final String[] pTensorCodenames = {
-            "comet",
-            "komodo",
-            "caiman",
-            "tokay",
-            "akita",
             "husky",
             "shiba",
             "felix",
@@ -909,24 +904,22 @@ public class ApplicationPackageManager extends PackageManager {
     public boolean hasSystemFeature(String name, int version) {
         String packageName = ActivityThread.currentPackageName();
         if (packageName != null
-                && (packageName.equals("com.google.android.googlequicksearchbox")
-                || packageName.equals("com.google.android.apps.nexuslauncher"))) {
+                && packageName.equals("com.google.android.googlequicksearchbox")) {
             if (Arrays.asList(featuresPixel).contains(name)) return true;
             if (Arrays.asList(featuresPixelOthers).contains(name)) return true;
             if (Arrays.asList(featuresTensor).contains(name)) return true;
             if (Arrays.asList(featuresNexus).contains(name)) return true;
+        }
+        if (packageName != null && Arrays.asList(featuresTensor).contains(name)
+                && !Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.product.device"))) {
+            return false;
         }
         if (packageName != null
                 && packageName.equals("com.google.android.apps.photos")
                 && SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true)) {
             if (Arrays.asList(featuresPixel).contains(name)) return false;
             if (Arrays.asList(featuresPixelOthers).contains(name)) return true;
-            if (Arrays.asList(featuresTensor).contains(name)) return false;
             if (Arrays.asList(featuresNexus).contains(name)) return true;
-        }
-        if (name != null && Arrays.asList(featuresTensor).contains(name)
-                && !Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.product.device"))) {
-            return false;
         }
         if (Arrays.asList(featuresPixel).contains(name)) return true;
         if (Arrays.asList(featuresPixelOthers).contains(name)) return true;
--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -1353,7 +1353,8 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        PixelPropsUtils.setProps(context);
+        String packageName = context.getPackageName();
+        PixelPropsUtils.setProps(packageName);
         return app;
     }
     
@@ -1371,7 +1372,8 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        PixelPropsUtils.setProps(context);
+        String packageName = context.getPackageName();
+        PixelPropsUtils.setProps(packageName);
         return app;
     }
 
