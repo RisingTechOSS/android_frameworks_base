91e609969899db3da4022fd7d6c042330a9ea6f4
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-30 16:06:52 +0800

    Constants: Update settings theming keys
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/theme/RisingSettingsConstants.java
+++ packages/SystemUI/src/com/android/systemui/theme/RisingSettingsConstants.java
@@ -19,12 +19,13 @@ import android.provider.Settings;
 
 public class RisingSettingsConstants {
     public static final String SETTINGS_THEME_STYLE = "settings_theme_style";
+    public static final String SETTINGS_HOMEPAGE_SHOWCASE = "settings_homepage_showcase";
+    public static final String SETTINGS_SHOW_MULTI_USER_AVATAR_ON_HOMEPAGE = "show_multi_user_avatar_on_homepage";
     public static final String QS_PANEL_TILE_HAPTIC = "qs_panel_tile_haptic";
     public static final String LOCKSCREEN_WIDGETS_ENABLED = "lockscreen_widgets_enabled";
     public static final String LOCKSCREEN_WIDGETS = "lockscreen_widgets";
     public static final String LOCKSCREEN_WIDGETS_EXTRAS = "lockscreen_widgets_extras";
     public static final String USER_SELECTED_RESOLUTION = "user_selected_resolution";
-    public static final String SHOW_MULTI_USER_AVATAR_ON_HOMEPAGE = "show_multi_user_avatar_on_homepage";
     public static final String STATUS_BAR_BATTERY_STYLE = Settings.System.STATUS_BAR_BATTERY_STYLE;
     public static final String QS_BATTERY_STYLE = Settings.System.QS_BATTERY_STYLE;
     public static final String QS_SHOW_BATTERY_PERCENT = Settings.System.QS_SHOW_BATTERY_PERCENT;
@@ -38,12 +39,13 @@ public class RisingSettingsConstants {
 
     public static final String[] SYSTEM_SETTINGS_KEYS = {
         SETTINGS_THEME_STYLE,
+        SETTINGS_HOMEPAGE_SHOWCASE,
+        SETTINGS_SHOW_MULTI_USER_AVATAR_ON_HOMEPAGE,
         QS_PANEL_TILE_HAPTIC,
         LOCKSCREEN_WIDGETS_ENABLED,
         LOCKSCREEN_WIDGETS,
         LOCKSCREEN_WIDGETS_EXTRAS,
         USER_SELECTED_RESOLUTION,
-        SHOW_MULTI_USER_AVATAR_ON_HOMEPAGE,
         STATUS_BAR_BATTERY_STYLE,
         QS_BATTERY_STYLE,
         QS_SHOW_BATTERY_PERCENT,
